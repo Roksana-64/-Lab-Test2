b43fad99a4e84f47cb779968f7e96922c0b59eb2
Roksana-64 <mostroksanaakter18@gmail.com>
2026-06-15 09:44:18 +0600

    Create Shape.java

---
+++ src/shapes/Shape.java
@@ -0,0 +1,26 @@
+package shapes; //difinding class under shape package
+
+public abstract class Shape {
+
+    private String  color; // we are making child class private
+    private boolean filled;
+
+    public Shape(String color, boolean filled) {
+        this.color  = color;
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    @Override
+    public String toString() {
+        return "Color : " + color + "\n" +
+               "Filled: " + (filled ? "Yes" : "No");
+    }
+
+    public String  getColor()           { return color;  }
+    public boolean isFilled()           { return filled; }
+    public void    setColor(String c)   { this.color  = c; }
+    public void    setFilled(boolean f) { this.filled = f; }
+}
